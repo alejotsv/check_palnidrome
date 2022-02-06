a38230fbc7d5fcc82a8068a2bcf8f578d82f225e
Alejandro Salas <asalas.wd@outlook.com>
2022-02-06 18:19:57 -0500

    create function according to the length of the string

--- check_palindrome.js
+++ check_palindrome.js
@@ -1,19 +1,46 @@
 function checkPalindrome(string) {
+  let isPalindrome = true;
   let lowerCase;
   let trimmed;
 
   console.log(string);
-  
-  // Set initial phrase to lowercase
   lowerCase = string.toLowerCase();
   console.log(lowerCase);
-
-  // Remove non-alphabetical characters (spaces, punctuation, numbers, etc.)
   trimmed = lowerCase.replace(/[^A-z]/g,"");
   console.log(trimmed);
 
+  console.log(trimmed.length);
+
+  let j;
+
+  // compare all characters if the length is even
+  if (trimmed.length%2 == 0){
+    j = trimmed.length/2;
+    for (let i=0; i<j; i++){
+      if (trimmed[i]!==trimmed[trimmed.length-1-i]){
+        isPalindrome = false;
+        break;
+      } else {
+        console.log(`${trimmed[i]} = ${trimmed[trimmed.length-1-i]}`);
+      }
+    }
+
+  // compare all characters except the one in the middle if the length is odd 
+  } else {
+    j = (trimmed.length - 1)/2;
+    console.log(j);
+    for (let i=0; i<j; i++){
+      if (trimmed[i]!==trimmed[trimmed.length-1-i]){
+        isPalindrome = false;
+        break;
+      } else {
+        console.log(`${trimmed[i]} = ${trimmed[trimmed.length-1-i]}`);
+      }
+    }
+  }
+
 }
 
-let toCheck = 'Esta asfafA ASKJF ~!!!-3';
+let toCheck = 'Salas';
 
 checkPalindrome(toCheck);
